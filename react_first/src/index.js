3b7ae9352e9abd7fd7296bf125ce4e9a883931c3
Jack <number1zero.jak@googlemail.com>
2022-04-14 15:58:23 +0100

    Created app with create-react-app

--- react_first/src/index.js
+++ react_first/src/index.js
@@ -15,3 +15,7 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello World');
+
+ReactDOM.render(element, document.getElementById('root'))
